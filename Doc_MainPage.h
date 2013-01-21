e4b529ad17d6192214d5bea557eea4e3c0c90e80
Mathieu Clément <mathieu.clement@freebourg.org>
2013-01-21 21:55:18 +0100

    Add main page content of documentation

---
+++ Doc_MainPage.h
@@ -0,0 +1,8 @@
+/*! \mainpage REX12 Team 03 K-Junior code documentation
+ *
+ * \section intro_sec Getting started
+ *
+ * If you switch to the "Files" page you will see all the files we wrote for the presentation
+ * and be able to read the documentation online.
+ *
+ */
